3ec2c70e6170c9286241c7b355f4063632151735
er-omsonar <er.omsonar@gmail.com>
2025-10-18 17:06:29 +0530

    localstorage implementation

--- todocontextLocal/src/App.jsx
+++ todocontextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -24,6 +24,17 @@ function App() {
     ? {...prevTodo, completed: !prevTodo.completed } : prevTodo
   ))
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+  
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
